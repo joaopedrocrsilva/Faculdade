191025882064a478b284395adb4d1ad47c4e99ff
joaopedrocrsilva <joaopedrocrsilva@hotmail.com>
2021-05-17 19:44:49 -0300

    main

--- Estudo_C/exercicio_resolvido6.c
+++ Estudo_C/exercicio_resolvido6.c
@@ -16,14 +16,10 @@ int main()
     gratificacao = salario_base * 0.05;
     imposto = salario_base * 0.07;
     
-    salario_final = salario_base - gratificacao - imposto;
+    salario_final = salario_base + gratificacao - imposto;
     
     //print values
     printf("Salario Final %2.f", salario_final);
-    
-    
-    
-    
     
     
     return 0;
