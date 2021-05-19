f532b0d02508a76690db078e0d6e35fd91d1a72f
joaopedrocrsilva <joaopedrocrsilva@hotmail.com>
2021-05-19 10:44:22 -0300

    main

--- Estudo_Cap3_C/exercicio_resolvivo8.c
+++ Estudo_Cap3_C/exercicio_resolvivo8.c
@@ -1,27 +1,27 @@
 //#include <stdio.h>
-//#include <math.h>
-
 
 int main()
 {
-  //set variables
-  int x;
-  int z;
-  
-  //enter values
-  scanf("%d", &z);
-  scanf("%d", &x);
-  
-  
-  //set variables
-  double y;
-  y = pow(z,2) - pow(x,2);
-  y = sqrt(y);
-  
-  
-  //print results
-  printf("Resultado: %f", y);
-  
-  
+    //set variables 
+    int valor_deposito;
+    int taxa_juros;
+    
+    //enter values
+    scanf("%d", &valor_deposito);
+    scanf("%d", &taxa_juros);
+    
+    //set variables
+    double valor_rendimento;
+    double valor_final;
+    
+    valor_rendimento = valor_deposito * taxa_juros/100; 
+    valor_final = valor_deposito + valor_rendimento;
+    
+    //print variables
+    printf("valor rendimento %2.f", valor_rendimento);
+    
+    printf("\nvalor final %2.f", valor_final);
+    
     return 0;
+    
 }
