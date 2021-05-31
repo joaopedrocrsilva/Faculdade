4a74a9228b54c31c0e9612220da43c7fdf10e7cd
joaopedrocrsilva <joaopedrocrsilva@hotmail.com>
2021-05-31 11:36:00 -0300

    mian

---
+++ Estudo_Cap4_C/exercicio_resolvido7.c
@@ -0,0 +1,26 @@
+//#include <stdio.h>
+//#include <math.h>
+
+int main(){
+    //set variable
+    int I;
+    float A;
+    float B;
+    float C;
+
+    //enter values
+    printf("Digite o número de I: ");
+    scanf("%d", &I);
+    printf("Digite o número de A: ");
+    scanf("%f", &A);
+    printf("Digite o número de B: ");
+    scanf("%f", &B);
+    printf("Digite o número de C: ");
+    scanf("%f", &C);
+
+
+    //set coditions
+    
+
+
+}
