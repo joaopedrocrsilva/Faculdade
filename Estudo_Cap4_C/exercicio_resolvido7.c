c401e4b779c5d12fdd34a2be0db37ac617e01675
joaopedrocrsilva <joaopedrocrsilva@hotmail.com>
2021-05-31 12:04:37 -0300

    main

--- Estudo_Cap4_C/exercicio_resolvido7.c
+++ Estudo_Cap4_C/exercicio_resolvido7.c
@@ -44,8 +44,43 @@ int main(){
         }
     }
 
+    if(I == 2){
+        if(num1 > num2 && num1 > num3){
+            if(num2 > num3){
+                printf("Ordem Decrescente: %2.f %2.f %2.f", num1, num2, num3);
+            }else{
+                printf("Ordem Decrescente: %2.f %2.f %2.f", num1, num3, num2);
+            }
+        }
+        if(num2 > num1 && num2 > num3){
+            if(num1 > num3){
+                printf("Ordem Decrescente: %2.f %2.f %2.f", num2, num1, num3);
+            }else{
+                printf("Ordem Decrescente: %2.f %2.f %2.f", num2, num3, num1);
+            }
+        }
+        if(num3 > num1 && num3 > num2){
+            if(num1 > num2){
+                printf("Ordem Decrescente: %2.f %2.f %2.f", num3, num1, num2);
+            }else{
+                printf("Ordem Decrescente: %2.f %2.f %2.f", num3, num2, num1);
+            }
+        }
+    }
 
+    if(I == 3){
+        if(num1 > num2 && num1 > num3){
+            printf("Imprimi: %2.f %2.f %2.f", num2, num1, num3);
+        }
+        if(num2 > num1 && num2 > num3){
+            printf("Imprimi: %2.f %2.f %2.f", num1, num2, num3);
+        }
+        if(num3 > num2 && num3 > num1){
+            printf("Imprimi: %2.f %2.f %2.f", num2, num3, num1);
+        }
+    }
 
-
-
+    
+    return 0;
+    
 }
