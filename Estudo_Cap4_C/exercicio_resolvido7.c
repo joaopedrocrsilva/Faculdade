29b373b1bdeb2b8a5c390e6d9ba48d2cd1a58a19
joaopedrocrsilva <joaopedrocrsilva@hotmail.com>
2021-05-31 11:41:18 -0300

    main

--- Estudo_Cap4_C/exercicio_resolvido7.c
+++ Estudo_Cap4_C/exercicio_resolvido7.c
@@ -4,22 +4,41 @@
 int main(){
     //set variable
     int I;
-    float A;
-    float B;
-    float C;
+    float num1;
+    float num2;
+    float num3;
 
     //enter values
     printf("Digite o número de I: ");
     scanf("%d", &I);
     printf("Digite o número de A: ");
-    scanf("%f", &A);
+    scanf("%f", &num1);
     printf("Digite o número de B: ");
-    scanf("%f", &B);
+    scanf("%f", &num2);
     printf("Digite o número de C: ");
-    scanf("%f", &C);
+    scanf("%f", &num3);
 
 
     //set coditions
+    if(I == 1){
+        if(num1 > num2 && num1 > num3){
+            if(num2 > num3){
+                printf("Ordem: %2.f %2.f %2.f", num1, num2, num3);
+            }
+        }
+        if(num2 > num1 && num2 > num3){
+            if(num1 > num3){
+                printf("Ordem: %2.f %2.f %2.f", num2, num1, num3);
+            }
+        }
+        if(num3 > num1 && num3 > num2){
+            if(num1 > num2){
+                printf("Ordem: %2.f %2.f %2.f", num3, num1, num2);
+            }
+        }
+    }
+
+
     
 
 
